20f1fff4ea81b4259a4028a793a4a3e698bcc9cc
arianiapalmer <arianiapalmer@gmail.com>
2023-06-10 19:50:14 -0400

    added styling

--- js/chart.js
+++ js/chart.js
@@ -3,9 +3,9 @@
 let canvasElem = document.getElementById('chart')
 
 /* TODO:
- * - Instantiate a new AppState -- done
- * - Use a method on that AppState to load vote data from localStorage. -- 
- * - Create a data object for chart.js using your AppState's allProducts array. -- 
+ * - Instantiate a new AppState 
+ * - Use a method on that AppState to load vote data from localStorage.
+ * - Create a data object for chart.js using your AppState's allProducts array. 
  * - Combine the data object with configuration information for chart.js type, colors, etc
  * - Call chart.js with the configuration and the canvasElem
 *
@@ -44,11 +44,20 @@ function renderChart() {
                 customcanvasBackgroundColor: {
                     color: 'white',
                 },
+
             },
             scales: {
+                x: {
+                    ticks: {
+                        color: 'white',
+                    }
+                },
                 y: {
+                    ticks: {
+                        color: 'white',
+                    },
                     beginAtZero: true
-                }
+                },
             }
         }
     }
